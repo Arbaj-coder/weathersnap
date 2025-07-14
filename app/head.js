80492a0a947bb280c68c35064410759cadf27b05
Arbaj-coder <2024kucp1060@iiitkota.ac.in>
2025-07-14 09:46:15 +0530

    Create head.js

---
+++ app/head.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "WeatherSnap : Plan your day with precision",
+  description: "A weather app that shows the current situation.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+}
